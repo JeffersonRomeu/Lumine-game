fe02a631395debdfadcda95144f6c3e288388fc0
Jefferson Romeu <jefferson_romeu@hotmail.com>
2019-02-12 17:51:33 -0200

    Add files via upload

---
+++ CameraView.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraView : MonoBehaviour {
+
+    public SpriteRenderer backGround;
+
+	void Start () {
+        Camera.main.fieldOfView = backGround.bounds.size.x * Screen.height / Screen.width * 0.5f;
+	}
+	
+}
---
+++ ScenarioMoving.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScenarioMoving : MonoBehaviour {
+
+    private float speed;
+    public static bool scenarioMoving;
+
+    private void FixedUpdate()
+    {
+        this.transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        if (scenarioMoving)
+        {
+            speed = -20f;
+            speed -= Time.deltaTime;
+        }
+        else
+        {
+            speed = 0;
+        }
+        limitSpeed();
+    }
+
+    public void limitSpeed()
+    {
+        if (speed <= -100f)
+        {
+            speed = -100f;
+        }
+    }
+}
